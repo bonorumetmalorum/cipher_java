71e096259aaf9fc4221053f8ed6893b1ffa798cf
Govind Venkatesh <venkateshgovind1@gmail.com>
2018-12-05 19:52:03 +0000

    removed file reading and made it all in one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        boolean run = true;
-
         Cipher cipher = cipherFactory(args);
 
-        while(run){
-            System.out.println("Encrypt (press E) / Decrypt (press D) / Quit (press Q)");
-            String in = System.console().readLine().toLowerCase();
-            switch (in) {
-                case "e":
-                    System.out.println("please input your text to be encrypted");
-                    String plaintext = System.console().readLine();
-                    String encrypt = cipher.encrypt(plaintext);
-                    System.out.println("your encrypted text: " + encrypt);
-                    break;
-                case "d":
-                    System.out.println("please input your text to be decrypted");
-                    String ciphertext = System.console().readLine();
-                    String decrypted = cipher.decrypt(ciphertext);
-                    System.out.println("your decrypted text: " + decrypted);
-                    break;
-                case "q": run = false; break;
-
-                default: System.out.println("invalid option"); break;
-
+        while(true){
+            System.out.println("input text to be encrypted or enter Q to quit");
+            String in = System.console().readLine();
+            if(!in.toLowerCase().equals("q")){
+                String encrypted = cipher.encrypt(in);
+                System.out.println("encrypted: " + encrypted);
+                String decrypted = cipher.decrypt(encrypted);
+                System.out.println("decrypted: " + decrypted);
+            }
+            else{
+                break;
             }
         }
     }
@@ -34,8 +23,7 @@ public class Main {
         int seed = Integer.parseInt(System.console().readLine());
         System.out.println("please input a threshold number (0.0 - 1.0)");
         float threshold = Float.parseFloat(System.console().readLine());
-        Cipher cipher = new Cipher(seed, threshold);
-        return cipher;
+        return new Cipher(seed, threshold);
     }
 
 
